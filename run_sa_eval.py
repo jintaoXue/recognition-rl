24b834d7b4af899a0014aba2bd65aae338cce5b6
CaiChicken233 <2868833285@qq.com>
2022-12-28 16:43:17 +0800

    .

--- run_sa_eval.py
+++ run_sa_eval.py
@@ -585,7 +585,7 @@ def _main() :
         interval = 1000
         for num in range(100, 101):
             # model_num = eval_end_num - num*interval
-            model_num = 125000
+            model_num = 200000
             config.description = 'recog_hr10act1__adaptive_background_downsample__bottleneck'
             models_sa.recog_rl__bottleneck__adaptive__given_number().update(config, model_num)
             env_master = gallery.evaluate__recog__one_background_downsample_bottleneck(config, mode)
