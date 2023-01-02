a81f85bae591da1e8b0a7e936ee2d75b89c5e027
CaiChicken233 <2868833285@qq.com>
2023-01-02 10:22:33 +0800

    .

--- core/recognition_net.py
+++ core/recognition_net.py
@@ -574,7 +574,7 @@ def pad_data(data: torch.Tensor, pad_size: torch.Size, pad_value=np.inf):
 
 def cut_state(state: rllib.basic.Data) :
     state_ = copy.deepcopy(state)
-    horizon = 10
+    horizon = 1
     raw_horizon = 10
     state_.ego = state_.ego[:,-horizon:,:]
     #change horizon stamp from raw_horizon to custom horizon
