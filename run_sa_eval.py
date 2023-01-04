98ec74d7784f4d1630be68bec57940e87aebf309
CaiChicken233 <2868833285@qq.com>
2023-01-04 13:21:02 +0800

    .

--- run_sa_eval.py
+++ run_sa_eval.py
@@ -566,7 +566,7 @@ def main():
             for other_svo in range(8,9):
                 config.description = 'evaluate' + '--recog_fix_{}_{}__one_background__bottleneck'.format(0.1*ego_svo, 0.1*other_svo)
                 models_sa.isac_recog__bottleneck__adaptive().update(config)
-                env_master = gallery.evaluate__fix_svo__new_one_background__bottleneck(config, 0.1*ego_svo, 0.1*other_svo,mode)
+                env_master = gallery.evaluate__recog_fix_svo__new_one_background__bottleneck(config, 0.1*ego_svo, 0.1*other_svo,mode)
                 env_master.create_tasks(func=run_one_episode)
                 ray.get([t.run.remote(n_iters=200) for t in env_master.tasks])
                 del env_master
