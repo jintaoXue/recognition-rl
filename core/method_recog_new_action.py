fc2ba6ecee0ce3323b8fca400ce9ac9073a3c319
CaiChicken233 <2868833285@qq.com>
2023-01-06 13:49:43 +0800

    .

--- core/method_recog_new_action.py
+++ core/method_recog_new_action.py
@@ -45,7 +45,7 @@ class RecogV1(MethodSingleAgent):
     buffer_size = 750000
     batch_size = 128
 
-    start_timesteps = 30000
+    start_timesteps = 50000
     # start_timesteps = 10  ## ! warning
     before_training_steps = 0
 
