bb8d724d70ae956103566e82bad019fc13d4809b
CaiChicken233 <2868833285@qq.com>
2023-02-07 15:19:04 +0800

    .

--- scripts/ma/evaluate_isac_bottleneck.sh
+++ scripts/ma/evaluate_isac_bottleneck.sh
@@ -3,6 +3,6 @@
 for fp in $(seq 0.0 .1 1.0)
 do
 echo 'start evaluate svo:'$fp; 
-CUDA_VISIBLE_DEVICES=$0 python run_sa_eval.py vtrue-1 --evaluate --svo $fp 
+CUDA_VISIBLE_DEVICES=$0 python run_ma_eval.py vtrue-1 --evaluate --svo $fp 
 
 done
