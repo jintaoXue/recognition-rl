0222909c5fdb12c77054de4ed3dfeee72c20c7cc
CaiChicken233 <2868833285@qq.com>
2023-02-21 10:16:31 +0800

    .

--- utils/agents_master.py
+++ utils/agents_master.py
@@ -102,8 +102,8 @@ class AgentListMaster(universe.AgentsMaster):
     # change in 2022/12/2
     def __init__(self, config: rllib.basic.YamlConfig, topology_map: TopologyMap, **kwargs):
         super().__init__(config, topology_map, **kwargs)
-        self.horizon = config.raw_horizon 
-        # self.horizon = 30
+        # self.horizon = config.raw_horizon 
+        self.horizon = 30
         perception_cls = config.get('perception_cls', Perception)
         self.perception = perception_cls(config, topology_map, self.dim_vehicle_state, self.horizon)
     class GetVehicleState(object):
